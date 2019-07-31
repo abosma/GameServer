29e3af85ce66a7f7eb3640749761ff8e6b25ce03
abosma <atillabosma@hotmail.com>
2019-07-31 23:43:54 +0200

    Added all properties based on https://pastebin.com/raw/nBBV0ta6

--- GameServerCore/Domain/ICharData.cs
+++ GameServerCore/Domain/ICharData.cs
@@ -1,4 +1,5 @@
-﻿using GameServerCore.Enums;
+﻿using System.Numerics;
+using GameServerCore.Enums;
 
 namespace LeagueSandbox.GameServer.Content
 {
@@ -6,31 +7,72 @@ namespace LeagueSandbox.GameServer.Content
     {
         float BaseHp { get; }
         float BaseMp { get; }
-        float BaseDamage { get; }
-        float AttackRange { get; }
-        int MoveSpeed { get; }
-        float Armor { get; }
-        float SpellBlock { get; }
-        float BaseStaticHpRegen { get; }
-        float BaseStaticMpRegen { get; }
-        float AttackDelayOffsetPercent { get; }
-        float AttackDelayCastOffsetPercent { get; }
         float HpPerLevel { get; }
         float MpPerLevel { get; }
-        float DamagePerLevel { get; }
-        float ArmorPerLevel { get; }
-        float SpellBlockPerLevel { get; }
         float HpRegenPerLevel { get; }
         float MpRegenPerLevel { get; }
+        float BaseStaticHpRegen { get; }
+        float BaseFactorHpRegen { get; }
+        float BaseStaticMpRegen { get; }
+        float BaseFactorMpRegen { get; }
+        float BaseDamage { get; }
+        float DamagePerLevel { get; }
+        float Armor { get; }
+        float ArmorPerLevel { get; }
+        float SpellBlock { get; }
+        float SpellBlockPerLevel { get; }
+        float BaseDodge { get; }
+        float DodgePerLevel { get; }
+        float BaseMissChance { get; }
+        float BaseCritChance { get; }
+        float CritPerLevel { get; }
+        float CritDamageBonus { get; }
+        int MoveSpeed { get; }
+        float AttackRange { get; }
+        float AttackAutoInterruptPercent { get; }
+        float AcquisitionRange { get; }
         float AttackSpeedPerLevel { get; }
-        bool IsMelee { get; } //Yes or no
+        float TowerTargetingPriority { get; }
+        float DeathTime { get; }
+        float ExpGivenOnDeath { get; }
+        float GoldGivenOnDeath { get; }
+        float ExperienceRadius { get; }
+        float GoldRadius { get; }
+        float DeathEventListeningRadius { get; }
+        bool LocalGoldSplitWithLastHitter { get; }
+        float LocalGoldGivenOnDeath { get; }
+        float LocalExpGivenonDeath { get; }
+        float GlobalGoldGivenOnDeath { get; }
+        float GlobalExpGivenOnDeath { get; }
+        float Significance { get; }
+        float AbilityPower { get; }
+        float AbilityPowerIncPerLevel { get; }
+        float PerceptionBubbleRadius { get; }
+        float OverrideCollisionHeight { get; }
+        float OverrideCollisionWidth { get; }
         float PathfindingCollisionRadius { get; }
         float GameplayCollisionRadius { get; }
-        PrimaryAbilityResourceType ParType { get; }
+        bool UseOverrideBoundingBox { get; }
+        Vector3 OverrideboundingBox { get; }
+        float BoundingCylinderRadius { get; }
+        float BoundingCylinderHeight { get; }
+        float BoundingSphereRadius { get; }
+        float OccludedUnitySelectableDistance { get; }
         string[] SpellNames { get; }
-        int[] MaxLevels { get; }
-        int[][] SpellsUpLevels { get; }
         string[] ExtraSpells { get; }
+        string[] AttackNames { get; }
+        string[] PassiveNames { get; }
+        string[] PassiveLuaNames { get; }
+        string CriticalAttackStr { get; }
+        int[] SpellMaxLevelsOverride { get; }
+        int[][] SpellsUpLevelsOverride { get; }
+        float[] AttackDelayCastOffsetPercentAttackSpeedRatio { get; }
+        float[] AttackDelayCastOffsetPercent { get; }
+        float[] AttackDelayOffsetPercent { get; }
+        float[] AttackProbability { get; }
+        float[] PassiveRanges { get; }
+        bool IsMelee { get; } //Yes or no
+        PrimaryAbilityResourceType ParType { get; }
         IPassiveData[] Passives { get; }
         void Load(string name);
     }
--- GameServerLib/Content/CharData.cs
+++ GameServerLib/Content/CharData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Numerics;
 using GameServerCore.Enums;
 using LeagueSandbox.GameServer.Logging;
 using log4net;
@@ -27,32 +28,66 @@ namespace LeagueSandbox.GameServer.Content
 
         public float BaseHp { get; private set; } = 100.0f;
         public float BaseMp { get; private set; } = 100.0f;
+        public float HpPerLevel { get; private set; } = 0.0f;
+        public float MpPerLevel { get; private set; } = 0.0f;
+        public float HpRegenPerLevel { get; private set; } = 0.0f;
+        public float MpRegenPerLevel { get; private set; } = 0.0f;
+        public float BaseStaticHpRegen { get; private set; } = 1.0f;
+        public float BaseFactorHpRegen { get; private set; } = 0.0f;
+        public float BaseStaticMpRegen { get; private set; } = 1.0f;
+        public float BaseFactorMpRegen { get; private set; } = 0.0f;
         public float BaseDamage { get; private set; } = 10.0f;
-        public float AttackRange { get; private set; } = 100.0f;
-        public int MoveSpeed { get; private set; } = 100;
+        public float DamagePerLevel { get; private set; } = 0.0f;
         public float Armor { get; private set; } = 1.0f;
-        public float SpellBlock { get; private set; }
-        public float BaseStaticHpRegen { get; private set; } = 0.30000001f;
-        public float BaseStaticMpRegen { get; private set; } = 0.30000001f;
-        public float AttackDelayOffsetPercent { get; private set; }
-        public float AttackDelayCastOffsetPercent { get; private set; }
-        public float HpPerLevel { get; private set; } = 10.0f;
-        public float MpPerLevel { get; private set; } = 10.0f;
-        public float DamagePerLevel { get; private set; } = 10.0f;
         public float ArmorPerLevel { get; private set; } = 1.0f;
-        public float SpellBlockPerLevel { get; private set; }
-        public float HpRegenPerLevel { get; private set; }
-        public float MpRegenPerLevel { get; private set; }
-        public float AttackSpeedPerLevel { get; private set; }
-        public bool IsMelee { get; private set; } //Yes or no
+        public float SpellBlock { get; private set; } = 0.0f;
+        public float SpellBlockPerLevel { get; private set; } = 0.0f;
+        public float BaseDodge { get; private set; } = 0.0f;
+        public float DodgePerLevel { get; private set; } = 0.0f;
+        public float BaseMissChance { get; private set; } = 0.0f;
+        public float BaseCritChance { get; private set; } = 0.0f;
+        public float CritPerLevel { get; private set; } = 0.0f;
+        public float CritDamageBonus { get; private set; } = 2.0f;
+        public int MoveSpeed { get; private set; } = 100;
+        public float AttackRange { get; private set; } = 100.0f;
+        public float AttackAutoInterruptPercent { get; private set; } = 0.2f;
+        public float AcquisitionRange { get; private set; } = 750.0f;
+        public float AttackSpeedPerLevel { get; private set; } = 0.0f;
+        public float TowerTargetingPriority { get; private set; } = 0.0f;
+        public float DeathTime { get; private set; } = -1.0f;
+        public float ExpGivenOnDeath { get; private set; } = 48.0f;
+        public float GoldGivenOnDeath { get; private set; } = 25.0f;
+        public float ExperienceRadius { get; private set; } = 0.0f;
+        public float GoldRadius { get; private set; } = 0.0f;
+        public float DeathEventListeningRadius { get; private set; } = 1000.0f;
+        public bool LocalGoldSplitWithLastHitter { get; private set; } = false;
+        public float LocalGoldGivenOnDeath { get; private set; } = 0.0f;
+        public float LocalExpGivenonDeath { get; private set; } = 0.0f;
+        public float GlobalGoldGivenOnDeath { get; private set; } = 0.0f;
+        public float GlobalExpGivenOnDeath { get; private set; } = 0.0f;
+        public float Significance { get; private set; } = 0.0f;
+        public float AbilityPower { get; private set; } = 0.0f;
+        public float AbilityPowerIncPerLevel { get; private set; } = 0.0f;
+        public float PerceptionBubbleRadius { get; private set; } = 1350.0f;
+        public float OverrideCollisionHeight { get; private set; } = -1.0f;
+        public float OverrideCollisionWidth { get; private set; } = -1.0f;
         public float PathfindingCollisionRadius { get; private set; } = -1.0f;
         public float GameplayCollisionRadius { get; private set; } = 65.0f;
-        public PrimaryAbilityResourceType ParType { get; private set; } = PrimaryAbilityResourceType.MANA;
+        public bool UseOverrideBoundingBox { get; private set; }
+        public Vector3 OverrideboundingBox { get; private set; }
+        public float BoundingCylinderRadius { get; private set; }
+        public float BoundingCylinderHeight { get; private set; }
+        public float BoundingSphereRadius { get; private set; }
+        public float OccludedUnitySelectableDistance { get; private set; } = 0.0f;
+        public string[] SpellNames { get; private set; } = {"", "", "", ""};
+        public string[] ExtraSpells { get; private set; } = {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+        public string[] AttackNames { get; private set; } = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+        public string[] PassiveNames { get; private set; } = {"", "", "", "", "", ""};
+        public string[] PassiveLuaNames { get; private set; } = { "", "", "", "", "", "" };
+        public string CriticalAttackStr { get; private set; }
+        public int[] SpellMaxLevelsOverride { get; private set; } = { 5, 5, 5, 3 };
 
-        public string[] SpellNames { get; private set; } = { "", "", "", "" };
-        public int[] MaxLevels { get; private set; } = { 5, 5, 5, 3 };
-
-        public int[][] SpellsUpLevels { get; private set; } =
+        public int[][] SpellsUpLevelsOverride { get; private set; } =
         {
             new[] {1, 3, 5, 7, 9, 99},
             new[] {1, 3, 5, 7, 9, 99},
@@ -60,7 +95,33 @@ namespace LeagueSandbox.GameServer.Content
             new[] {6, 11, 16, 99, 99, 99}
         };
 
-        public string[] ExtraSpells { get; private set; } = { "", "", "", "", "", "", "", "" };
+        public float[] AttackDelayCastOffsetPercentAttackSpeedRatio { get; private set; } =
+        {
+            1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f
+        };
+
+        public float[] AttackDelayCastOffsetPercent { get; private set; } =
+        {
+            0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+        };
+
+        public float[] AttackDelayOffsetPercent { get; private set; } =
+        {
+            0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+        };
+
+        public float[] AttackProbability { get; private set; } =
+        {
+            2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f
+        };
+
+        public float[] PassiveRanges { get; private set; }
+
+        //public float AttackDelayOffsetPercent { get; private set; }
+        //public float AttackDelayCastOffsetPercent { get; private set; }
+        public bool IsMelee { get; private set; } //Yes or no
+        
+        public PrimaryAbilityResourceType ParType { get; private set; } = PrimaryAbilityResourceType.MANA;
 
         public IPassiveData[] Passives { get; private set; } =
         {
@@ -92,29 +153,49 @@ namespace LeagueSandbox.GameServer.Content
 
             BaseHp = file.GetFloat("Data", "BaseHP", BaseHp);
             BaseMp = file.GetFloat("Data", "BaseMP", BaseMp);
-            BaseDamage = file.GetFloat("Data", "BaseDamage", BaseDamage);
-            AttackRange = file.GetFloat("Data", "AttackRange", AttackRange);
-            MoveSpeed = file.GetInt("Data", "MoveSpeed", MoveSpeed);
-            Armor = file.GetFloat("Data", "Armor", Armor);
-            SpellBlock = file.GetFloat("Data", "SpellBlock", SpellBlock);
-            BaseStaticHpRegen = file.GetFloat("Data", "BaseStaticHPRegen", BaseStaticHpRegen);
-            BaseStaticMpRegen = file.GetFloat("Data", "BaseStaticMPRegen", BaseStaticMpRegen);
-            AttackDelayOffsetPercent = file.GetFloat("Data", "AttackDelayOffsetPercent", AttackDelayOffsetPercent);
-            AttackDelayCastOffsetPercent = file.GetFloat("Data", "AttackDelayCastOffsetPercent", AttackDelayCastOffsetPercent);
+
             HpPerLevel = file.GetFloat("Data", "HPPerLevel", HpPerLevel);
             MpPerLevel = file.GetFloat("Data", "MPPerLevel", MpPerLevel);
-            DamagePerLevel = file.GetFloat("Data", "DamagePerLevel", DamagePerLevel);
-            ArmorPerLevel = file.GetFloat("Data", "ArmorPerLevel", ArmorPerLevel);
-            SpellBlockPerLevel = file.GetFloat("Data", "SpellBlockPerLevel", SpellBlockPerLevel);
+
             HpRegenPerLevel = file.GetFloat("Data", "HPRegenPerLevel", HpRegenPerLevel);
             MpRegenPerLevel = file.GetFloat("Data", "MPRegenPerLevel", MpRegenPerLevel);
+
+            BaseStaticHpRegen = file.GetFloat("Data", "BaseStaticHPRegen", BaseStaticHpRegen);
+            BaseFactorHpRegen = file.GetFloat("Data", "BaseFactorHPRegen", BaseFactorHpRegen);
+            BaseStaticMpRegen = file.GetFloat("Data", "BaseStaticMPRegen", BaseStaticMpRegen);
+            BaseFactorMpRegen = file.GetFloat("Data", "BaseFactorMPRegen", BaseFactorMpRegen);
+
+            BaseDamage = file.GetFloat("Data", "BaseDamage", BaseDamage);
+            DamagePerLevel = file.GetFloat("Data", "DamagePerLevel", DamagePerLevel);
+
+            Armor = file.GetFloat("Data", "Armor", Armor);
+            ArmorPerLevel = file.GetFloat("Data", "ArmorPerLevel", ArmorPerLevel);
+
+            SpellBlock = file.GetFloat("Data", "SpellBlock", SpellBlock);
+            SpellBlockPerLevel = file.GetFloat("Data", "SpellBlockPerLevel", SpellBlockPerLevel);
+
+            BaseDodge = file.GetFloat("Data", "BaseDodge", BaseDodge);
+            DodgePerLevel = file.GetFloat("Data", "LevelDodge", DodgePerLevel);
+
+            BaseCritChance = file.GetFloat("Data", "BaseCritChance", BaseCritChance);
+            CritPerLevel = file.GetFloat("Data", "CritPerLevel", CritPerLevel);
+            CritDamageBonus = file.GetFloat("Data", "CritDamageBonus", CritDamageBonus);
+
+            AttackRange = file.GetFloat("Data", "AttackRange", AttackRange);
+            AcquisitionRange = file.GetFloat("Data", "AcquisitionRange", AcquisitionRange);
+
+            MoveSpeed = file.GetInt("Data", "MoveSpeed", MoveSpeed);
+
             AttackSpeedPerLevel = file.GetFloat("Data", "AttackSpeedPerLevel", AttackSpeedPerLevel);
+            AttackDelayOffsetPercent[0] = file.GetFloat("Data", "AttackDelayOffsetPercent", AttackDelayOffsetPercent[0]);
+            AttackDelayCastOffsetPercent[0] = file.GetFloat("Data", "AttackDelayCastOffsetPercent", AttackDelayCastOffsetPercent[0]);
             IsMelee = file.GetString("Data", "IsMelee", IsMelee ? "true" : "false").Equals("true");
-            PathfindingCollisionRadius =
-                file.GetFloat("Data", "PathfindingCollisionRadius", PathfindingCollisionRadius);
+
+            PathfindingCollisionRadius = file.GetFloat("Data", "PathfindingCollisionRadius", PathfindingCollisionRadius);
             GameplayCollisionRadius = file.GetFloat("Data", "GameplayCollisionRadius", GameplayCollisionRadius);
-            Enum.TryParse<PrimaryAbilityResourceType>(file.GetString("Data", "PARType", ParType.ToString()),
-                out var tempPar);
+
+            Enum.TryParse<PrimaryAbilityResourceType>(file.GetString("Data", "PARType", ParType.ToString()), out var tempPar);
+
             ParType = tempPar;
 
             for (var i = 0; i < 4; i++)
@@ -124,10 +205,11 @@ namespace LeagueSandbox.GameServer.Content
 
             for (var i = 0; i < 4; i++)
             {
-                SpellsUpLevels[i] = file.GetIntArray("Data", $"SpellsUpLevels{i + 1}", SpellsUpLevels[i]);
+                SpellsUpLevelsOverride[i] = file.GetIntArray("Data", $"SpellsUpLevels{i + 1}", SpellsUpLevelsOverride[i]);
             }
 
-            MaxLevels = file.GetIntArray("Data", "MaxLevels", MaxLevels);
+            SpellMaxLevelsOverride = file.GetIntArray("Data", "MaxLevels", SpellMaxLevelsOverride);
+
             for (var i = 0; i < 8; i++)
             {
                 ExtraSpells[i] = file.GetString("Data", $"ExtraSpell{i + 1}", ExtraSpells[i]);
